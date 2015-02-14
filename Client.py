3909ab9f381c299b7fe8dee96cf92d264b3d0c55
mishef10 <mishef10@mail.ru>
2015-02-14 09:20:20 +0300

    nickname(nw)

--- Client.py
+++ Client.py
@@ -8,12 +8,8 @@ def receive():
         tex.insert(1.0, data.decode()+'\n')
 
 def send(event):
-    text=nickname+': '+ent.get()
+    text=ent.get()
     sock.send(text.encode())
-    ent.delete(0,END)
-def set_nick(event):
-    nick=nick_ent.get()
-    return nick
 
 sock = socket.socket()
 sock.connect(('localhost', 7313))
@@ -22,27 +18,21 @@ th=Thread(target=receive, args=())
 th.start()
 
 root=Tk()
-win = Toplevel(root,relief=SUNKEN,bg="blue")
-win.title("Nickname")
-win.minsize(width=300,height=50)
-nick_ent=Entry(win, width=35)
-nick_but=Button(win, text="Set Nickname")
-nickname='nickname'
-nick_but.bind("<Button-1>", set_nick)
-print(nickname)
-nick_ent.grid(row=0, column=0)
-nick_but.grid(row=0, column=4, padx=10)
 tex=Text(root, width=50, height=10, font=12, wrap=WORD)
 scr = Scrollbar(root,command=tex.yview)
 tex.configure(yscrollcommand=scr.set)
-tex.grid(row=0,column=0, pady=23)
-scr.grid(row=0,column=1, sticky=N+S)
+tex.grid(row=1,column=0,pady=23)
+scr.grid(row=1, column=1,sticky=N+S)
 ent = Entry(root,width=70)
 ent.grid(row=6,column=0,)
 but=Button(root, text="Send")
 but.grid(row=6,column=6)
 but.bind("<Button-1>", send)
 ent.bind("<Return>", send)
+nick_ent=Entry(root, width=45)
+nick_ent.grid(row=0,column=0,)
+nick_but=Button(root, text="set nick")
+nick_but.grid(row=0, column=1,)
 
 mainloop()
 
